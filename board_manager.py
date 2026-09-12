993af099804a060d078bfa3bdc4066702d3b8b93
kbedgood <kbedgood@ku.edu>
2026-09-12 17:23:25 -0500

    Kaitlyn board_manager.py functions and .gitignore

--- board_manager.py
+++ board_manager.py
@@ -22,24 +22,75 @@ Remember to add in-code comments:
 
 """
 
-from cell import Cell
+from cell_real import Cell #changed to match cell_real.py
 
 class BoardManager:
     """
     Tasks: Board Management (Kaitlyn) & Mine Placement (Lauren)
     """
     def __init__(self):
-        # TODO (Kaitlyn): Initialize a 10x10 2D array of Cell objects.
-        # TODO (Kaitlyn): Keep column (A-J) and row (1-10) labels in mind for setup.
-        pass
+        # (Kaitlyn): Initialize a 10x10 2D array of Cell objects.
+        # (Kaitlyn): Keep column (A-J) and row (1-10) labels in mind for setup.
+        self.rows = 10
+        self.cols = 10
+        self.grid = []
+
+        for row in range(self.rows):
+            new_row = []
+
+            for col in range(self.cols):
+                new_row.append(Cell())
+
+            self.grid.append(new_row)
 
     def get_cell(self, row: int, col: int) -> Cell:
-        # TODO (Kaitlyn): Return the Cell object at the given coordinates.
-        pass
+        # (Kaitlyn): Return the Cell object at the given coordinates.
+        return self.grid[row][col]
     
     def get_neighbors(self, row: int, col: int) -> list:
-        # TODO (Kaitlyn): Return a list of valid adjacent Cell objects.
-        pass
+        # (Kaitlyn): Return a list of valid adjacent Cell objects.
+        neighbors = []
+        if row < 0 or row >= self.rows:
+            return neighbors
+
+        if col < 0 or col >= self.cols:
+            return neighbors
+
+        for row_offset in range(-1, 2):
+            for col_offset in range(-1, 2):
+                neighbor_row = row+ row_offset
+                neighbor_col = col + col_offset
+
+                if row_offset != 0 or col_offset != 0:
+                    if neighbor_row >= 0 and neighbor_row < self.rows:
+                        if neighbor_col >=0  and neighbor_col < self.cols:
+                            neighbors.append(self.grid[neighbor_row][neighbor_col])
+
+        return neighbors
+
+    def print_board(self):
+        """
+        print_board needs to finished once cell_real.py is finished
+        need to print out the cell object and not just a "*"
+        will need something like this
+        for col in range(self.cols):
+            print(" " + str(self.grid[row][col]) + " |", end="")
+        
+        """
+        #label column
+        print("    A   B   C   D   E   F   G   H   I   J")
+        print("   +---+---+---+---+---+---+---+---+---+---+")
+
+        #print each row of board
+        for row in range(self.rows):
+            print(str(row + 1).rjust(2) + " |", end="")
+
+            for col in range(self.cols):
+                print(" * |", end="")
+
+            print()
+            print("   +---+---+---+---+---+---+---+---+---+---+")
+
 
     def place_mines(self, num_mines: int, first_row: int, first_col: int) -> None:
         # TODO (Lauren): Validate num_mines is between 10-20.
@@ -63,3 +114,4 @@ WHAT CAN CHANGE:
 - Label Mechanics (Kaitlyn): Decide most efficient way to manage required A-J and 1-10 labels
 
 """
+
