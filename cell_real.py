fdd31f2f8d86ac19898a8a899899a6b77e255178
kbedgood <kbedgood@ku.edu>
2026-09-17 19:20:01 -0500

    Cleaned up comments

--- cell_real.py
+++ cell_real.py
@@ -1,8 +1,8 @@
 """
-Module Name: cell.py
+Module Name: cell_real.py
 Class Name: Cell
 
-FIXME DELETE LATER: Greeshma, please fill in the following:
+
 
 Description: Represents one cell in the Minesweeper game board and stores its state, mine status, and number of adjacent mines.
 Inputs: None
@@ -13,15 +13,6 @@ Creation Date: September 17, 2026
 External Sources:
 """
 
-
-"""
-FIXME DELETE LATER
-
-Remember to add in-code comments:
-- comment major code blocks and/or individual lines to explain functionality
-- indicate if code is origina, sourced, combined
-
-"""
 
 class Cell:
     """
@@ -42,17 +33,4 @@ class Cell:
         self.adjacent_mines = 0
 
 
-"""
-FIXME DELETE LATER
 
-DO NOT CHANGE:
-- FUNCTION DEFINITIONS OR RETURN TYPES
-
-- Class Name: must remain "Cell"
-- Attribute Names: state, is_mine, and adjacent_mines
-- Attribute Types: state must remain an integer mapped to 0-3, is_mine must remain a boolean, adjacent_mines must be an integer
-
-WHAT CAN CHANGE:
-- Implementation code: replace pass statement with logic setting up the default states
-
-"""
